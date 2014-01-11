11d04d965cf683a7d3080a18e736d079a710c7c1
dantheother <dan@redgum.com.au>
2014-01-12 00:13:33 +1100

    shuffled some files around

--- chuffler/Web/Scripts/build/index.js
+++ chuffler/Web/Scripts/build/index.js
@@ -1,5 +1,67 @@
 ﻿/** @jsx React.DOM */
+
+var faketree = {
+  title: "howdy",
+  childNodes: [
+    {title: "bobby"},
+    {title: "suzie", childNodes: [
+      {title: "puppy", childNodes: [
+        {title: "dog house"}
+      ]},
+      {title: "cherry tree"}
+    ]}
+  ]
+};
+
+var Root = React.createClass({displayName: 'Root',
+	getInitialState: function() {
+		return {
+			tree:faketree
+		};
+	},
+  render: function() {
+    return (
+      TreeNode( {node:this.state.tree} )
+    );
+  }
+});
+
+var TreeNode = React.createClass({displayName: 'TreeNode',
+  getInitialState: function() {
+    return {
+      visible: true
+    };
+  },
+  render: function() {
+    var childNodes;
+    if (this.props.node.childNodes != null) {
+      childNodes = this.props.node.childNodes.map(function(node) {
+        return React.DOM.li(null, TreeNode( {node:node} ))
+      },this);
+    }
+
+    var style = {};
+    if (!this.state.visible) {
+      style.display = "none";
+    }
+
+    return (
+      React.DOM.div(null, 
+        React.DOM.h5( {onClick:this.toggle}, 
+          this.props.node.title
+        ),
+        React.DOM.ul( {style:style}, 
+          childNodes
+        )
+      )
+    );
+  },
+  toggle: function() {
+    this.setState({visible: !this.state.visible});
+  }
+});
+
 React.renderComponent(
-  React.DOM.p(null, "Hello, world from autocompile????!!!!"),
+  Root(null ),
   document.getElementById('main')
 );
--- chuffler/Web/Scripts/src/index.jsx
+++ chuffler/Web/Scripts/src/index.jsx
@@ -1,5 +1,67 @@
 ﻿/** @jsx React.DOM */
+
+var faketree = {
+  title: "howdy",
+  childNodes: [
+    {title: "bobby"},
+    {title: "suzie", childNodes: [
+      {title: "puppy", childNodes: [
+        {title: "dog house"}
+      ]},
+      {title: "cherry tree"}
+    ]}
+  ]
+};
+
+var Root = React.createClass({
+	getInitialState: function() {
+		return {
+			tree:faketree
+		};
+	},
+  render: function() {
+    return (
+      <TreeNode node={this.state.tree} />
+    );
+  }
+});
+
+var TreeNode = React.createClass({
+  getInitialState: function() {
+    return {
+      visible: true
+    };
+  },
+  render: function() {
+    var childNodes;
+    if (this.props.node.childNodes != null) {
+      childNodes = this.props.node.childNodes.map(function(node) {
+        return <li><TreeNode node={node} /></li>
+      },this);
+    }
+
+    var style = {};
+    if (!this.state.visible) {
+      style.display = "none";
+    }
+
+    return (
+      <div>
+        <h5 onClick={this.toggle}>
+          {this.props.node.title}
+        </h5>
+        <ul style={style}>
+          {childNodes}
+        </ul>
+      </div>
+    );
+  },
+  toggle: function() {
+    this.setState({visible: !this.state.visible});
+  }
+});
+
 React.renderComponent(
-  <p>Hello, world from autocompile????!!!!</p>,
+  <Root />,
   document.getElementById('main')
 );
